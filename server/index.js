8c99eaf7feb78ab2c3e1802996be738ef88bdcbd
Mahmud Oloyede <oloyedemahmud@gmail.com>
2023-07-11 23:00:59 +0100

    Completed Gift List

--- server/index.js
+++ server/index.js
@@ -8,14 +8,14 @@ app.use(express.json());
 
 // TODO: hardcode a merkle root here representing the whole nice list
 // paste the hex string in here, without the 0x prefix
-const MERKLE_ROOT = '';
+const MERKLE_ROOT = '4cd4d68371c54912d60190f9b55f8bbd5833dd678200aa69a15a325638b41e61';
 
 app.post('/gift', (req, res) => {
   // grab the parameters from the front-end here
   const body = req.body;
 
   // TODO: prove that a name is in the list 
-  const isInTheList = false;
+  const isInTheList = verifyProof(body.proof, body.name, MERKLE_ROOT);
   if(isInTheList) {
     res.send("You got a toy robot!");
   }
